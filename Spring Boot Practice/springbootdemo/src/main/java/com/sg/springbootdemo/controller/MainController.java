2affd8a6089a8bbf993e1a7111ae81f75fd62f17
Teresa de Castro <decastro.ter@gmail.com>
2022-10-28 12:13:57 -0400

    Finished first part.

--- Spring Boot Practice/springbootdemo/src/main/java/com/sg/springbootdemo/controller/MainController.java
+++ Spring Boot Practice/springbootdemo/src/main/java/com/sg/springbootdemo/controller/MainController.java
@@ -5,9 +5,13 @@
  */
 package com.sg.springbootdemo.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 /**
  *
@@ -26,5 +30,27 @@ public class MainController {
         model.addAttribute("firstName", name);
 
         return "test";
+    }
+
+    @PostMapping("testForm")
+    public String testForm(HttpServletRequest request) {
+        name = request.getParameter("formFirstName");
+        number = Integer.parseInt(request.getParameter("formNumber"));
+
+        return "redirect:/test";
+    }
+
+    @GetMapping("testList")
+    public String testList(Model model) {
+        List<Integer> numbers = new ArrayList<>();
+
+        numbers.add(0);
+        numbers.add(10);
+        numbers.add(6);
+        numbers.add(35);
+
+        model.addAttribute("numberList", numbers);
+
+        return "testList";
     }
 }
