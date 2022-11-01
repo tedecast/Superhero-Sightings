96bd0506dfdd02007fdccc77b53472ad871ffd8f
Teresa de Castro <decastro.ter@gmail.com>
2022-11-01 13:06:08 -0400

    Need to figure out the testing.

--- Class Roster Practice/classroster/src/main/java/com/sg/dao/CourseDao.java
+++ Class Roster Practice/classroster/src/main/java/com/sg/dao/CourseDao.java
@@ -16,17 +16,17 @@ import java.util.List;
  */
 public interface CourseDao {
 
-    Course getCourseById(int id);
+    public Course getCourseById(int id);
 
-    List<Course> getAllCourses();
+    public List<Course> getAllCourses();
 
-    Course addCourse(Course course);
+    public Course addCourse(Course course);
 
-    void updateCourse(Course course);
+    public void updateCourse(Course course);
 
-    void deleteCourseById(int id);
+    public void deleteCourseById(int id);
 
-    List<Course> getCoursesForTeacher(Teacher teacher);
+    public List<Course> getCoursesForTeacher(Teacher teacher);
 
-    List<Course> getCoursesForStudent(Student student);
+    public List<Course> getCoursesForStudent(Student student);
 }
