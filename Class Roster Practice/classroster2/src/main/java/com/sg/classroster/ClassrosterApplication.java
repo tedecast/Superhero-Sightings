1890e5eef3b5e8746569f950ccf79e5d1cd0cdc8
Teresa de Castro <decastro.ter@gmail.com>
2022-11-02 11:37:26 -0400

    Finished renaming + started index page

--- Class Roster Practice/classroster2/src/main/java/com/sg/classroster/ClassrosterApplication.java
+++ Class Roster Practice/classroster2/src/main/java/com/sg/classroster/ClassrosterApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Classroster2Application {
+public class ClassrosterApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Classroster2Application.class, args);
+		SpringApplication.run(ClassrosterApplication.class, args);
 	}
 
 }
