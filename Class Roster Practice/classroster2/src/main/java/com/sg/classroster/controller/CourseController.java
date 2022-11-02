6d683b35c0747f2a38fce9e853f9fab22118d411
Teresa de Castro <decastro.ter@gmail.com>
2022-11-02 12:18:42 -0400

    Details work.

--- Class Roster Practice/classroster2/src/main/java/com/sg/classroster/controller/CourseController.java
+++ Class Roster Practice/classroster2/src/main/java/com/sg/classroster/controller/CourseController.java
@@ -11,11 +11,14 @@ import com.sg.classroster.dao.TeacherDao;
 import com.sg.classroster.entities.Course;
 import com.sg.classroster.entities.Student;
 import com.sg.classroster.entities.Teacher;
+import java.util.ArrayList;
 import java.util.List;
+import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 /**
  *
@@ -42,6 +45,30 @@ public class CourseController {
         model.addAttribute("teachers", teachers);
         model.addAttribute("students", students);
         return "courses";
+    }
+
+    @PostMapping("addCourse")
+    public String addCourse(Course course, HttpServletRequest request) {
+        String teacherId = request.getParameter("teacherId");
+        String[] studentIds = request.getParameterValues("studentId");
+
+        course.setTeacher(teacherDao.getTeacherById(Integer.parseInt(teacherId)));
+
+        List<Student> students = new ArrayList<>();
+        for (String studentId : studentIds) {
+            students.add(studentDao.getStudentById(Integer.parseInt(studentId)));
+        }
+        course.setStudents(students);
+        courseDao.addCourse(course);
+
+        return "redirect:/courses";
+    }
+
+    @GetMapping("courseDetail")
+    public String courseDetail(Integer id, Model model) {
+        Course course = courseDao.getCourseById(id);
+        model.addAttribute("course", course);
+        return "courseDetail";
     }
 
 }
