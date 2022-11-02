727eb4e411f50b5775a6211601391f668d68e0ae
Teresa de Castro <decastro.ter@gmail.com>
2022-11-02 12:13:14 -0400

    Started Courses

---
+++ Class Roster Practice/classroster2/src/main/java/com/sg/classroster/controller/CourseController.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sg.classroster.controller;
+
+import com.sg.classroster.dao.CourseDao;
+import com.sg.classroster.dao.StudentDao;
+import com.sg.classroster.dao.TeacherDao;
+import com.sg.classroster.entities.Course;
+import com.sg.classroster.entities.Student;
+import com.sg.classroster.entities.Teacher;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ *
+ * @author Teresa
+ */
+@Controller
+public class CourseController {
+
+    @Autowired
+    TeacherDao teacherDao;
+
+    @Autowired
+    StudentDao studentDao;
+
+    @Autowired
+    CourseDao courseDao;
+
+    @GetMapping("courses")
+    public String displayCourses(Model model) {
+        List<Course> courses = courseDao.getAllCourses();
+        List<Teacher> teachers = teacherDao.getAllTeachers();
+        List<Student> students = studentDao.getAllStudents();
+        model.addAttribute("courses", courses);
+        model.addAttribute("teachers", teachers);
+        model.addAttribute("students", students);
+        return "courses";
+    }
+
+}
