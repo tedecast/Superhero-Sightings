822dc751b1d94af42f622691da4071fdb228e5b6
Teresa de Castro <decastro.ter@gmail.com>
2022-11-02 12:52:07 -0400

    Added validation in for students

--- Class Roster Practice/classroster2/src/main/java/com/sg/classroster/controller/StudentController.java
+++ Class Roster Practice/classroster2/src/main/java/com/sg/classroster/controller/StudentController.java
@@ -10,9 +10,11 @@ import com.sg.classroster.dao.StudentDao;
 import com.sg.classroster.dao.TeacherDao;
 import com.sg.classroster.entities.Student;
 import java.util.List;
+import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -63,7 +65,10 @@ public class StudentController {
     }
 
     @PostMapping("editStudent")
-    public String performEditStudent(Student student) {
+    public String performEditStudent(@Valid Student student, BindingResult result) {
+        if (result.hasErrors()) {
+            return "editStudent";
+        }
         studentDao.updateStudent(student);
         return "redirect:/students";
     }
