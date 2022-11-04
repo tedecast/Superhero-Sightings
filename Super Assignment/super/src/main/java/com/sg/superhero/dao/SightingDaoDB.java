bc07567383ccdc27bde4fb0c9edb5785f12fa50d
Teresa de Castro <decastro.ter@gmail.com>
2022-11-04 14:30:00 -0400

    started superdaodb

---
+++ Super Assignment/super/src/main/java/com/sg/superhero/dao/SightingDaoDB.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sg.superhero.dao;
+
+import com.sg.superhero.entities.Location;
+import com.sg.superhero.entities.Sighting;
+import com.sg.superhero.entities.Super;
+import java.time.LocalDate;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author Teresa
+ */
+@Repository
+public class SightingDaoDB implements SightingDao {
+    
+    @Autowired
+    JdbcTemplate jdbc;
+
+    @Override
+    public Sighting getSightingByID(int sightingID) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Sighting> getAllSightings() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Sighting addSighting(Sighting sighting) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void updateSighting(Sighting sighting) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void deleteSightingByID(int sightingID) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Sighting> getSightingsForLocation(Location location) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Sighting> getSightingsByDate(LocalDate date) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Sighting> getSightingsBySuper(Super supers) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
