f0e3300e5cfbfce409ca8b101141873155403601
Teresa de Castro <decastro.ter@gmail.com>
2022-11-04 10:37:35 -0400

    Added in sightingDao

---
+++ Superhero Assignment/superhero/src/main/java/com/sg/dao/SightingDao.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sg.dao;
+
+import com.sg.entities.Sighting;
+import java.util.List;
+
+/**
+ *
+ * @author Teresa
+ */
+public interface SightingDao {
+
+    public Sighting getSightingByID(int sightingID);
+
+    public List<Sighting> getAllSightings();
+
+    public Sighting addSighting(Sighting sighting);
+
+    public void updateSighting(Sighting sighting);
+
+    public void deleteSightingByID(int sightingID);
+
+}
