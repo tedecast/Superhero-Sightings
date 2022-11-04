7aa2f0be5dd4b60285e83b65ccc340e828ec385d
Teresa de Castro <decastro.ter@gmail.com>
2022-11-04 13:01:42 -0400

    completed up to organizationId

---
+++ Super Assignment/super/src/main/java/com/sg/dao/OrganizationDaoDB.java
@@ -0,0 +1,94 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sg.dao;
+
+import com.sg.entities.Organization;
+import com.sg.entities.Super;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ *
+ * @author Teresa
+ */
+@Repository
+public class OrganizationDaoDB implements OrganizationDao {
+
+    @Autowired
+    JdbcTemplate jdbc;
+
+    @Override
+    public Organization getOrganizationByID(int organizationID) {
+        try {
+            final String SELECT_ORGANIZATION_BY_ID = "SELECT * FROM organization WHERE organizationID = ?";
+            
+            return this.jdbc.queryForObject(SELECT_ORGANIZATION_BY_ID, new OrganizationMapper(), organizationID);
+        } catch (DataAccessException ex){
+            return null;
+        }
+    }
+
+    @Override
+    public List<Organization> getAllOrganizations() {
+        final String SELECT_ALL_ORGANIZATIONS = "SELECT * FROM organization";
+        return this.jdbc.query(SELECT_ALL_ORGANIZATIONS, new OrganizationMapper());
+    }
+
+    @Override
+    @Transactional
+    public Organization addOrganization(Organization organization) {
+        final String INSERT_ORGANIZATION = "INSERT INTO organization (name, description, address, contactInfo, type)"
+                + "VALUES(?,?,?,?,?)";
+        this.jdbc.update(INSERT_ORGANIZATION, 
+                organization.getName(), 
+                organization.getDescription(), 
+                organization.getAddress(), 
+                organization.getContactInfo(), 
+                organization.getType());
+        
+        int newOrganizationID = this.jdbc.queryForObject("SELECT LAST_INSERT_ORGANIZATIONID()", Integer.class);
+        organization.setOrganizationID(newOrganizationID);
+        return organization;
+    }
+
+    @Override
+    public void updateOrganization(Organization organization) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void deleteOrganizationByID(int organizationID) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Organization> getOrganizationsForSuper(Super superhero) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public static final class OrganizationMapper implements RowMapper<Organization> {
+
+        @Override
+        public Organization mapRow(ResultSet rs, int index) throws SQLException {
+            Organization organization = new Organization();
+            organization.setOrganizationID(rs.getInt("organizationID"));
+            organization.setName(rs.getString("name"));
+            organization.setDescription(rs.getString("description"));
+            organization.setAddress(rs.getString("address"));
+            organization.setContactInfo(rs.getString("contactInfo"));
+            organization.setType(rs.getString("type"));
+            return organization;
+        }
+    }
+
+}
