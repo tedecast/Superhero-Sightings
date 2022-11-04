8aef3cb26416edcf2da37a14d2d320a8aeb93f40
Teresa de Castro <decastro.ter@gmail.com>
2022-11-04 14:51:46 -0400

    Added in mapper.

--- Super Assignment/super/src/main/java/com/sg/superhero/dao/SightingDaoDB.java
+++ Super Assignment/super/src/main/java/com/sg/superhero/dao/SightingDaoDB.java
@@ -8,10 +8,13 @@ package com.sg.superhero.dao;
 import com.sg.superhero.entities.Location;
 import com.sg.superhero.entities.Sighting;
 import com.sg.superhero.entities.Super;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.time.LocalDate;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -64,4 +67,16 @@ public class SightingDaoDB implements SightingDao {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
+    public static final class SightingMapper implements RowMapper<Sighting> {
+        
+        @Override
+        public Sighting mapRow(ResultSet rs, int index) throws SQLException {
+            Sighting sighting = new Sighting();
+            sighting.setSightingID(rs.getInt("sightingID"));
+            sighting.setDate(rs.getDate("date").toLocalDate());
+            sighting.setDescription(rs.getString("description"));
+            
+            return sighting;
+        }
+    }
 }
