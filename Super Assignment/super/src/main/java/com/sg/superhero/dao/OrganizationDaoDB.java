a3e22d4febfcd41e3427722a4cebb2d8e0d635fb
Teresa de Castro <decastro.ter@gmail.com>
2022-11-06 16:49:36 -0500

    Completed organizationdaodb for now. will go back later to clean

--- Super Assignment/super/src/main/java/com/sg/superhero/dao/OrganizationDaoDB.java
+++ Super Assignment/super/src/main/java/com/sg/superhero/dao/OrganizationDaoDB.java
@@ -5,8 +5,11 @@
  */
 package com.sg.superhero.dao;
 
+import com.sg.superhero.dao.SuperDaoDB.SuperMapper;
+import com.sg.superhero.dao.SuperpowerDaoDB.SuperpowerMapper;
 import com.sg.superhero.entities.Organization;
 import com.sg.superhero.entities.Super;
+import com.sg.superhero.entities.Superpower;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
@@ -23,79 +26,127 @@ import org.springframework.transaction.annotation.Transactional;
  */
 @Repository
 public class OrganizationDaoDB implements OrganizationDao {
-
+    
     @Autowired
     JdbcTemplate jdbc;
-
+    
     @Override
     public Organization getOrganizationByID(int organizationID) {
         try {
             final String SELECT_ORGANIZATION_BY_ID = "SELECT * FROM organization WHERE organizationID = ?";
             
             return this.jdbc.queryForObject(SELECT_ORGANIZATION_BY_ID, new OrganizationMapper(), organizationID);
-        } catch (DataAccessException ex){
+        } catch (DataAccessException ex) {
             return null;
         }
     }
-
+    
     @Override
     public List<Organization> getAllOrganizations() {
         final String SELECT_ALL_ORGANIZATIONS = "SELECT * FROM organization";
         return this.jdbc.query(SELECT_ALL_ORGANIZATIONS, new OrganizationMapper());
     }
-
+    
+    private void insertSuperOrganization(Organization organization) {
+        final String INSERT_SUPER_ORGANIZATION = "INSERT INTO "
+                + "SuperOrganization(superID, organizationID) VALUES (?,?)";
+        
+        for (Super superhero : organization.getSupers()) {
+            this.jdbc.update(INSERT_SUPER_ORGANIZATION,
+                    superhero.getSuperID(),
+                    organization.getOrganizationID());
+        }
+    }
+    
     @Override
     @Transactional
     public Organization addOrganization(Organization organization) {
         final String INSERT_ORGANIZATION = "INSERT INTO organization (name, description, address, contactInfo, type)"
                 + "VALUES(?,?,?,?,?)";
-        this.jdbc.update(INSERT_ORGANIZATION, 
-                organization.getName(), 
-                organization.getDescription(), 
-                organization.getAddress(), 
-                organization.getContactInfo(), 
+        this.jdbc.update(INSERT_ORGANIZATION,
+                organization.getName(),
+                organization.getDescription(),
+                organization.getAddress(),
+                organization.getContactInfo(),
                 organization.getType());
         
         int newOrganizationID = this.jdbc.queryForObject("SELECT LAST_INSERT_ORGANIZATIONID()", Integer.class);
         organization.setOrganizationID(newOrganizationID);
         
+        this.insertSuperOrganization(organization);
+        
         return organization;
     }
-
+    
     @Override
-    @Transactional
     public void updateOrganization(Organization organization) {
         final String UPDATE_ORGANIZATION = "UPDATE organization SET name = ?, description = ?, address = ?, "
                 + "contactInfo = ?, type = ? WHERE organizationID = ?";
         
-        this.jdbc.update(UPDATE_ORGANIZATION, 
-                organization.getOrganizationID(), 
-                organization.getName(), 
-                organization.getDescription(), 
-                organization.getAddress(), 
-                organization.getContactInfo(), 
+        this.jdbc.update(UPDATE_ORGANIZATION,
+                organization.getOrganizationID(),
+                organization.getName(),
+                organization.getDescription(),
+                organization.getAddress(),
+                organization.getContactInfo(),
                 organization.getType());
         
-//        final String DELETE_SUPER_ORGANIZATION = "DELETE FROM superOrganization WHERE organizationID = ?";
-//        this.jdbc.update(DELETE_SUPER_ORGANIZATION, organization.getOrganizationID());
+        final String DELETE_SUPER_ORGANIZATION = "DELETE FROM superOrganization WHERE organizationID = ?";
+        this.jdbc.update(DELETE_SUPER_ORGANIZATION, organization.getOrganizationID());
+        this.insertSuperOrganization(organization);
     }
-
+    
     @Override
+    @Transactional
     public void deleteOrganizationByID(int organizationID) {
-       final String DELETE_ORGANIZATION_SUPER = "DELETE FROM superOrganization WHERE organizationID = ?";
-       this.jdbc.update(DELETE_ORGANIZATION_SUPER, organizationID);
-       
-       final String DELETE_ORGANIZATION = "DELETE FROM organization where organizationID = ?";
-       this.jdbc.update(DELETE_ORGANIZATION, organizationID);
+        final String DELETE_ORGANIZATION_SUPER = "DELETE FROM superOrganization WHERE organizationID = ?";
+        this.jdbc.update(DELETE_ORGANIZATION_SUPER, organizationID);
+        
+        final String DELETE_ORGANIZATION = "DELETE FROM organization where organizationID = ?";
+        this.jdbc.update(DELETE_ORGANIZATION, organizationID);
+    }
+    
+    private Superpower getSuperpowerForSuper(int superpowerID) {
+        try {
+            final String SELECT_SP_FOR_SUPER = "SELECT sp.superpowerID, sp.name, sp.description FROM Superpower sp "
+                    + "JOIN Super s ON sp.superpowerID = s.superpowerID WHERE s.superpowerID = ?";
+            return this.jdbc.queryForObject(SELECT_SP_FOR_SUPER, new SuperpowerMapper(), superpowerID);
+        } catch (DataAccessException ex) {
+            return null;
+        }
+    }
+    
+    private List<Super> getSupersForOrganization(Organization organization) {
+        final String GET_SUPERS_FOR_ORG = "SELECT s.superID, s.superpowerID, s.type, s.name, s.description "
+                + "FROM SuperOrganization so "
+                + "JOIN Super s ON so.superID = s.superID "
+                + "WHERE so.organizationID = ?";
+        
+        List<Super> supers = this.jdbc.query(GET_SUPERS_FOR_ORG, new SuperMapper(), organization.getOrganizationID());
+        for (Super superhero : supers) {
+            superhero.setSuperpower(this.getSuperpowerForSuper(superhero.getSuperID()));
+        }
+        return supers;
     }
 
+//    private void associateSupers(List<Organization> organizations) {
+//        for (Organization organization : organizations) {
+//            organization.setSupers(this.getOrganizationsForSuper(organization.getOrganizationID()));
+//        }
+//    }
     @Override
     public List<Organization> getOrganizationsForSuper(Super superhero) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        final String SELECT_ORG_FOR_SUPER = "SELECT o.* FROM organization o JOIN "
+                + "SuperOrganization so ON so.organizationID = o.organizationID WHERE so.superID = ?";
+        List<Organization> organizations = this.jdbc.query(SELECT_ORG_FOR_SUPER, new OrganizationMapper(), superhero.getSuperID());
+        for (Organization organization : organizations) {
+            organization.setSupers(this.getSupersForOrganization(organization));
+        }
+        return organizations;
     }
-
+    
     public static final class OrganizationMapper implements RowMapper<Organization> {
-
+        
         @Override
         public Organization mapRow(ResultSet rs, int index) throws SQLException {
             Organization organization = new Organization();
@@ -108,5 +159,5 @@ public class OrganizationDaoDB implements OrganizationDao {
             return organization;
         }
     }
-
+    
 }
