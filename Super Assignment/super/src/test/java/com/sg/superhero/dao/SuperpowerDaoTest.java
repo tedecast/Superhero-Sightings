612078c38b0d697cd33c559a5aaa636d173d8f1e
Teresa de Castro <decastro.ter@gmail.com>
2022-11-06 18:53:54 -0500

    Started superpower test

---
+++ Super Assignment/super/src/test/java/com/sg/superhero/dao/SuperpowerDaoTest.java
@@ -0,0 +1,100 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sg.superhero.dao;
+
+import com.sg.superhero.entities.Superpower;
+import java.util.List;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ *
+ * @author Teresa
+ */
+@SpringBootTest
+public class SuperpowerDaoTest {
+    
+    public SuperpowerDaoTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getSuperpowerByID method, of class SuperpowerDao.
+     */
+    @Test
+    public void testGetSuperpowerByID() {
+    }
+
+    /**
+     * Test of getAllSuperpowers method, of class SuperpowerDao.
+     */
+    @Test
+    public void testGetAllSuperpowers() {
+    }
+
+    /**
+     * Test of addSuperpower method, of class SuperpowerDao.
+     */
+    @Test
+    public void testAddSuperpower() {
+    }
+
+    /**
+     * Test of updateSuperpower method, of class SuperpowerDao.
+     */
+    @Test
+    public void testUpdateSuperpower() {
+    }
+
+    /**
+     * Test of deleteSuperpowerByID method, of class SuperpowerDao.
+     */
+    @Test
+    public void testDeleteSuperpowerByID() {
+    }
+
+    public class SuperpowerDaoImpl implements SuperpowerDao {
+
+        public Superpower getSuperpowerByID(int superpowerID) {
+            return null;
+        }
+
+        public List<Superpower> getAllSuperpowers() {
+            return null;
+        }
+
+        public Superpower addSuperpower(Superpower superpower) {
+            return null;
+        }
+
+        public void updateSuperpower(Superpower superpower) {
+        }
+
+        public void deleteSuperpowerByID(int superpowerID) {
+        }
+    }
+    
+}
