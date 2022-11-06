43c38500f144504270d4a455167432b29bf67118
Teresa de Castro <decastro.ter@gmail.com>
2022-11-06 13:00:29 -0500

    Added in superdaodbtest just to make sure springboottest is working

---
+++ Super Assignment/super/src/test/java/com/sg/superhero/dao/SuperDaoDBTest.java
@@ -0,0 +1,105 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sg.superhero.dao;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ *
+ * @author Teresa
+ */
+@SpringBootTest
+public class SuperDaoDBTest {
+    
+    public SuperDaoDBTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getSuperByID method, of class SuperDaoDB.
+     */
+    @Test
+    public void testGetSuperByID() {
+    }
+
+    /**
+     * Test of getAllSupers method, of class SuperDaoDB.
+     */
+    @Test
+    public void testGetAllSupers() {
+    }
+
+    /**
+     * Test of addSuper method, of class SuperDaoDB.
+     */
+    @Test
+    public void testAddSuper() {
+    }
+
+    /**
+     * Test of updateSuper method, of class SuperDaoDB.
+     */
+    @Test
+    public void testUpdateSuper() {
+    }
+
+    /**
+     * Test of deleteSuperByID method, of class SuperDaoDB.
+     */
+    @Test
+    public void testDeleteSuperByID() {
+    }
+
+    /**
+     * Test of getSupersForSighting method, of class SuperDaoDB.
+     */
+    @Test
+    public void testGetSupersForSighting() {
+    }
+
+    /**
+     * Test of getSupersForSuperpower method, of class SuperDaoDB.
+     */
+    @Test
+    public void testGetSupersForSuperpower() {
+    }
+
+    /**
+     * Test of getSupersForLocation method, of class SuperDaoDB.
+     */
+    @Test
+    public void testGetSupersForLocation() {
+    }
+
+    /**
+     * Test of getSupersForOrganization method, of class SuperDaoDB.
+     */
+    @Test
+    public void testGetSupersForOrganization() {
+    }
+    
+}
