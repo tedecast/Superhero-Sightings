2be547193b80d835dafa3217055bf00fb1a0005d
Teresa de Castro <decastro.ter@gmail.com>
2022-11-08 09:35:25 -0500

    Starting OrganizationTest

---
+++ Super Assignment/super/src/test/java/com/sg/superhero/dao/OrganizationDaoDBTest.java
@@ -0,0 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sg.superhero.dao;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ *
+ * @author Teresa
+ */
+@SpringBootTest
+public class OrganizationDaoDBTest {
+    
+    public OrganizationDaoDBTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getOrganizationByID method, of class OrganizationDaoDB.
+     */
+    @Test
+    public void testGetOrganizationByID() {
+    }
+
+    /**
+     * Test of getAllOrganizations method, of class OrganizationDaoDB.
+     */
+    @Test
+    public void testGetAllOrganizations() {
+    }
+
+    /**
+     * Test of addOrganization method, of class OrganizationDaoDB.
+     */
+    @Test
+    public void testAddOrganization() {
+    }
+
+    /**
+     * Test of updateOrganization method, of class OrganizationDaoDB.
+     */
+    @Test
+    public void testUpdateOrganization() {
+    }
+
+    /**
+     * Test of deleteOrganizationByID method, of class OrganizationDaoDB.
+     */
+    @Test
+    public void testDeleteOrganizationByID() {
+    }
+
+    /**
+     * Test of getOrganizationsForSuper method, of class OrganizationDaoDB.
+     */
+    @Test
+    public void testGetOrganizationsForSuper() {
+    }
+    
+}
