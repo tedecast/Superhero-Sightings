109f35c9a7805cc2ccae6498e0416b4c7dce8ce0
Teresa de Castro <decastro.ter@gmail.com>
2022-11-14 14:31:40 -0500

    Figured out drop down menu

--- Super Assignment/super/src/main/java/com/sg/superhero/controller/SuperController.java
+++ Super Assignment/super/src/main/java/com/sg/superhero/controller/SuperController.java
@@ -10,8 +10,12 @@ import com.sg.superhero.dao.OrganizationDao;
 import com.sg.superhero.dao.PowerDao;
 import com.sg.superhero.dao.SightingDao;
 import com.sg.superhero.dao.SuperDao;
+import com.sg.superhero.entities.Super;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 /**
  *
@@ -19,6 +23,7 @@ import org.springframework.stereotype.Controller;
  */
 @Controller
 public class SuperController {
+
     @Autowired
     SuperDao superDao;
 
@@ -33,5 +38,12 @@ public class SuperController {
 
     @Autowired
     SightingDao sightingDao;
-    
+
+    @GetMapping("supers")
+    public String displayTeachers(Model model) {
+        List<Super> supers = this.superDao.getAllSupers();
+        model.addAttribute("supers", supers);
+        return "supers";
+    }
+
 }
