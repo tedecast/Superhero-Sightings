61da53b8ebd44382bc81e7fed532b18c2c76abee
Teresa de Castro <decastro.ter@gmail.com>
2022-11-16 12:01:23 -0500

    moved detailsOrg to details folder

--- Super Assignment/super/src/main/java/com/sg/superhero/controller/OrganizationController.java
+++ Super Assignment/super/src/main/java/com/sg/superhero/controller/OrganizationController.java
@@ -86,7 +86,7 @@ public class OrganizationController {
         return "redirect:/organizations";
     }
     
-    @GetMapping("detailsOrg")
+    @GetMapping("/details/detailsOrg")
     public String displayDetailsOrganization (HttpServletRequest request, Model model){
         int orgID = Integer.parseInt(request.getParameter("organizationID"));
         
