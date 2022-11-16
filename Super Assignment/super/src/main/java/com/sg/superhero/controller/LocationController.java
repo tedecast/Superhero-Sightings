99d1e4f1539e14faf4d84483da4c5589129ba845
Teresa de Castro <decastro.ter@gmail.com>
2022-11-16 14:29:40 -0500

    Figured out how to add in locations

--- Super Assignment/super/src/main/java/com/sg/superhero/controller/LocationController.java
+++ Super Assignment/super/src/main/java/com/sg/superhero/controller/LocationController.java
@@ -8,10 +8,12 @@ package com.sg.superhero.controller;
 import com.sg.superhero.entities.Location;
 import com.sg.superhero.service.SuperService;
 import java.util.List;
+import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 /**
  *
@@ -28,6 +30,27 @@ public class LocationController {
         List<Location> locations = this.service.getAllLocations();
         model.addAttribute("locations", locations);
         return "locations";
+    }
+    
+    @PostMapping("addLocation")
+    public String addLocation(HttpServletRequest request) {
+
+        String locationName = request.getParameter("locationName");
+        String locationDescription = request.getParameter("locationDescription");
+        String locationAddress = request.getParameter("locationAddress");
+        String latitude = request.getParameter("latitude");
+        String longitude = request.getParameter("longitude");
+
+        Location location = new Location();
+        location.setName(locationName);
+        location.setDescription(locationDescription);
+        location.setAddress(locationAddress);
+        location.setLatitude(latitude);
+        location.setLongitude(longitude);
+
+        this.service.addLocation(location);
+
+        return "redirect:/locations";
     }
 
 }
