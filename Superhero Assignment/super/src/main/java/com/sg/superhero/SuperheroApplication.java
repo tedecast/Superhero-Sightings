2b4a9363cfccec232db5e3804a90d6532d12bc49
Teresa de Castro <decastro.ter@gmail.com>
2023-05-14 20:19:16 -0400

    Added add screenshot

--- Superhero Assignment/super/src/main/java/com/sg/superhero/SuperheroApplication.java
+++ Superhero Assignment/super/src/main/java/com/sg/superhero/SuperheroApplication.java
@@ -1,6 +1,5 @@
 package com.sg.superhero;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SuperheroApplication {
 
     public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.load();
         SpringApplication.run(SuperheroApplication.class, args);
     }
 }
